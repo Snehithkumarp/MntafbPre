392ee33841840ddc61ce3d635ccfc9c67aa02459
mntcloudop <mntcloudX@gmail.com>
2024-10-19 15:15:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+EqutKIHl4vE1Mzll')
+GRP_LNK = environ.get('GRP_LNK', '')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+Jir1TAiii0M2MGI1')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Watch_Download_MnT_Link')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+EqutKIHl4vE1Mzll') # Support Chat Link Without https:// or @
