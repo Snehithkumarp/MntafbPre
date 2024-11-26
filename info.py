06419991a52c37bed25eae3ae88039e90d13be59
Snehithkumarp <125585610+Snehithkumarp@users.noreply.github.com>
2024-11-26 14:28:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://moviesplus-mpt.koyeb.app/")
+URL = environ.get("URL", "https://mntlinks-join-mov-ser.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
